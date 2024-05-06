d53e207799b630e3051a36f7632c972bfdbbc8aa
AmineDjab <114061385+AmineDjab@users.noreply.github.com>
2024-05-06 15:38:41 +0200

    Add files via upload

---
+++ __main__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from processer.processer import plotPersistenceDiagram
+from analyser.analyser import processImage
+
+plotPersistenceDiagram(processImage("C:/Users/djab-/OneDrive/Bureau/Projet recherche/E494/dsm",30)[0])
